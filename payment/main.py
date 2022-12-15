71562ca6bd398d7230f6e0a89c28e83e871972a0
Federico <federicolucaspontoriero@gmail.com>
2022-12-15 17:47:04 -0300

    backend and react project template

--- payment/main.py
+++ payment/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.background import BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
 from starlette.requests import Request
@@ -35,8 +36,13 @@ class Order(HashModel):
         database = redis
 
 
+@app.get("/orders/{pk}")
+def get(pk: str):
+    return Order.get(pk)
+
+
 @app.post("/orders")
-async def create(request: Request):  # id, quantity
+async def create(request: Request, background_tasks: BackgroundTasks):  # id, quantity
     body = await request.json()
 
     req = requests.get("http://localhost:8000/products/%s" % body["id"])
@@ -52,7 +58,7 @@ async def create(request: Request):  # id, quantity
     )
     order.save()
 
-    order_completed(order)
+    background_tasks.add_task(order_completed, order)
 
     return order
 
@@ -60,3 +66,4 @@ async def create(request: Request):  # id, quantity
 def order_completed(order: Order):
     order.status = "completed"
     order.save()
+    redis.xadd("order_completed", order.dict(), "*")
